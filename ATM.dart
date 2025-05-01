f8e5d54af2c92ae2f968c6278248a1857cabf1c6
AjayModhavadiya <amodhavadiya51@gmail.com>
2025-05-01 14:29:52 +0530

    changes

--- ATM.dart
+++ ATM.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 void main() {
   const int validPin = 1234;
   double totalBalance = 50000.0;
@@ -7,23 +8,35 @@ void main() {
   stdout.write("Enter your ATM PIN: ");
   String? pinInput = stdin.readLineSync();
   int? pin = int.tryParse(pinInput ?? '');
+
   if (pin == validPin) {
     print(" PIN Verified. Welcome, $name!");
-    stdout.write("Enter amount to withdraw: ");
-    String? amountInput = stdin.readLineSync();
-    double? amount = double.tryParse(amountInput ?? '');
-    if (amount != null) {
-      if (amount >= 1 && amount <= 50000) {
-        totalBalance -= amount;
-        print(" Withdrawal successful.");
-        print(" Remaining Balance: $totalBalance ");
-      } else {
-        print(" Not a valid amount.");
-      }
-    } else {
-      print(" Invalid input. Please enter a numeric value.");
+    stdout.write('1. Check Balance \n 2. Deposit \n 3. Withdraw \n 4. Exit \n');
+    stdout.write('Choose an option (1-4): ');
+    int? num = int.parse(stdin.readLineSync()!);
+    if (num == 1) {
+      print("your belance is $totalBalance");
+    } 
+    else if (num == 2) {
+      stdout.write("enter your diposit ammount :-");
+      int amount = int.parse(stdin.readLineSync()!);
+      print("your amount is :- $amount");
+      print(totalBalance += amount);
     }
-  } else {
-    print(" Invalid PIN.");
+     else if (num == 3) 
+     {
+      stdout.write("enter your withdraw ammount :-");
+      int amo = int.parse(stdin.readLineSync()!);
+      print("your withdraw ammount is $amo");
+      print(totalBalance -= amo);
+    }
+     else 
+    {
+      print("tankyou for visit plz come next time ");
+    }
+  }
+   else 
+  {
+    print("invalid pin");
   }
 }
